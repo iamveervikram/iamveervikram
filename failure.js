981295624d088ea8382b490d3d9c078d0cd79db1
Veer Vikram <67200634+iamveervikram@users.noreply.github.com>
2025-08-08 17:18:34 +0530

    Add files via upload

---
+++ failure.js
@@ -0,0 +1,7 @@
+//***************************trying to get json data from node.js to the html folder****************************
+// $.ajax({
+//   url: '/failure.html',
+//   complete: function(data) {
+//     console.log(data);
+//   }
+// });
